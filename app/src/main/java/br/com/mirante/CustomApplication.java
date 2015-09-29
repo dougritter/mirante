4d75b84b96845504029eda3d64b62d6f8e083149
dougritter <drmachado@gmail.com>
2015-09-28 22:47:44 -0300

    added parse key

--- app/src/main/java/br/com/mirante/CustomApplication.java
+++ app/src/main/java/br/com/mirante/CustomApplication.java
@@ -1,7 +1,19 @@
 package br.com.mirante;
 
+import android.app.Application;
+
+import com.parse.Parse;
+
 /**
  * Created by douglasritter on 9/28/15.
  */
-public class CustomApplication {
+public class CustomApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(this, BuildConfig.PARSE_APP_ID, BuildConfig.PARSE_CLIENT_KEY);
+    }
+
+
 }
