baf8c10ba17be78c71ddff31527cd72dca14ecd9
dougritter <drmachado@gmail.com>
2015-10-13 20:48:26 -0300

    added crashlytics

--- app/src/main/java/br/com/mirante/CustomApplication.java
+++ app/src/main/java/br/com/mirante/CustomApplication.java
@@ -3,15 +3,17 @@ package br.com.mirante;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by douglasritter on 9/28/15.
  */
 public class CustomApplication extends Application {
 
-    @Override
-    public void onCreate() {
+    @Override public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         Parse.initialize(this, BuildConfig.PARSE_APP_ID, BuildConfig.PARSE_CLIENT_KEY);
     }
 
--- app/src/main/java/br/com/mirante/MainActivity.java
+++ app/src/main/java/br/com/mirante/MainActivity.java
@@ -5,33 +5,23 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.parse.ParseObject;
+
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        testParse();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void testParse(){
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "barr");
+        testObject.saveInBackground();
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
