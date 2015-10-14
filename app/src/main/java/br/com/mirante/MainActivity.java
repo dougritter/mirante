d4a1480059440b703e80377d95d2b6770bf97c83
dougritter <drmachado@gmail.com>
2015-10-13 22:18:03 -0300

    added a form to create a channel (comunidade) no the same screen

--- app/src/main/java/br/com/mirante/MainActivity.java
+++ app/src/main/java/br/com/mirante/MainActivity.java
@@ -6,11 +6,14 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 import com.parse.SignUpCallback;
 
 import butterknife.Bind;
@@ -21,9 +24,14 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = MainActivity.class.getSimpleName();
 
+    // Create account
     @Bind(R.id.personName) TextView personName;
-    @Bind(R.id.email) TextView email;
-    @Bind(R.id.password) TextView password;
+    @Bind(R.id.email) EditText email;
+    @Bind(R.id.password) EditText password;
+
+    // Create channel
+    @Bind(R.id.channel) EditText channel;
+
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,15 +58,41 @@ public class MainActivity extends AppCompatActivity {
                 if (e == null) {
                     // Hooray! Let them use the app now.
                     Log.e(LOG_TAG, "Hooray! Let them use the app now");
+                    Toast.makeText(getApplicationContext(), "Hooray! Let them use the app now", Toast.LENGTH_SHORT).show();
 
                 } else {
                     // Sign up didn't succeed. Look at the ParseException
                     // to figure out what went wrong
                     Log.e(LOG_TAG, "Sign up didn't succeed. Look at the ParseException");
+                    Toast.makeText(getApplicationContext(), "Sign up didn't succeed. Look at the ParseException", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
+    }
+
+    @OnClick(R.id.createChannelButton) void createChannel(View view){
+        String CHANNEL = "Channel";
+        String CHANNEL_NAME = "channel_name";
+
+        ParseObject parseObject = new ParseObject(CHANNEL);
+        parseObject.put(CHANNEL_NAME, channel.getText().toString());
+
+        parseObject.saveInBackground(new SaveCallback() {
+            @Override public void done(ParseException e) {
+                if (e == null) {
+                    Log.e(LOG_TAG, "Channel " + channel.getText().toString() + " saved");
+                    Toast.makeText(getApplicationContext(), "Channel "+channel.getText().toString()
+                            + " saved", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Log.e(LOG_TAG, "Error: something went wrong saving Channel " + channel.getText().toString());
+                    Toast.makeText(getApplicationContext(), "Error: something went wrong saving Channel "
+                            +channel.getText().toString(), Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
 
     }
 
