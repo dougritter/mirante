2da992f966ad9fa50c845ce5afc04ecb80de0f71
dougritter <drmachado@gmail.com>
2015-12-14 22:22:49 -0200

    still trying

--- app/src/main/java/br/com/mirante/views/activities/HomeActivity.java
+++ app/src/main/java/br/com/mirante/views/activities/HomeActivity.java
@@ -23,6 +23,7 @@ public class HomeActivity extends AppCompatActivity {
     @Bind(R.id.newUserContainer) TextInputLayout newUserContainer;
     @Bind(R.id.newPasswordContainer) TextInputLayout newPasswordContainer;
 
+
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
